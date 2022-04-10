7891473ff76860ebf79a47416cb738f7500c5fa1
Ewy <Ewyboy@gmail.com>
2022-04-10 21:30:14 +0200

    1.18.2 Ready!

--- fabric/src/main/java/com/ewyboy/barista/client/GameBar.java
+++ fabric/src/main/java/com/ewyboy/barista/client/GameBar.java
@@ -12,12 +12,12 @@ public class GameBar {
         for (BarModule module : JsonHandler.barConfig.getModuleList()) {
             if (module.isDisplay()) {
                 switch (module.getName()) {
-                    case "text": ModuleHandler.getText(builder, module.getContext()); break;
-                    case "mods": ModuleHandler.getMods(builder); break;
-                    case "clock": ModuleHandler.getClock(builder, module.getContext()); break;
-                    case "session": ModuleHandler.getSession(builder); break;
-                    case "fps": ModuleHandler.getFps(mc, builder); break;
-                    case "memory": ModuleHandler.getMemory(builder); break;
+                    case "text" -> ModuleHandler.getText(builder, module.getContext());
+                    case "mods" -> ModuleHandler.getMods(builder);
+                    case "clock" -> ModuleHandler.getClock(builder, module.getContext());
+                    case "session" -> ModuleHandler.getSession(builder);
+                    case "fps" -> ModuleHandler.getFps(mc, builder);
+                    case "memory" -> ModuleHandler.getMemory(builder);
                 }
             }
         }
@@ -30,25 +30,25 @@ public class GameBar {
         for (BarModule module : JsonHandler.barConfig.getModuleList()) {
             if (module.isDisplay()) {
                 switch (module.getName()) {
-                    case "text": ModuleHandler.getText(builder, module.getContext()); break;
-                    case "mods": ModuleHandler.getMods(builder); break;
-                    case "clock": ModuleHandler.getClock(builder, module.getContext()); break;
-                    case "session": ModuleHandler.getSession(builder); break;
-                    case "day": ModuleHandler.getDay(mc, builder); break;
-                    case "time": ModuleHandler.getTime(mc, builder); break;
-                    case "fps": ModuleHandler.getFps(mc, builder); break;
-                    case "ping": ModuleHandler.getPing(mc, builder); break;
-                    case "memory": ModuleHandler.getMemory(builder); break;
-                    case "biome": ModuleHandler.getBiome(mc, builder); break;
-                    case "weather": ModuleHandler.getWeather(mc, builder); break;
-                    case "position": ModuleHandler.getPosition(mc, builder); break;
-                    case "chunk": ModuleHandler.getChunk(mc, builder); break;
-                    case "dimension": ModuleHandler.getDimension(mc, builder); break;
-                    case "facing": ModuleHandler.getFacing(mc, builder); break;
-                    case "looking_at": ModuleHandler.getLookingAt(mc, builder); break;
-                    case "property": ModuleHandler.getProperty(mc, builder); break;
-                    case "target": ModuleHandler.getTarget(mc, builder); break;
-                    case "target_health": ModuleHandler.getTargetHealth(mc, builder); break;
+                    case "text" -> ModuleHandler.getText(builder, module.getContext());
+                    case "mods" -> ModuleHandler.getMods(builder);
+                    case "clock" -> ModuleHandler.getClock(builder, module.getContext());
+                    case "session" -> ModuleHandler.getSession(builder);
+                    case "day" -> ModuleHandler.getDay(mc, builder);
+                    case "time" -> ModuleHandler.getTime(mc, builder);
+                    case "fps" -> ModuleHandler.getFps(mc, builder);
+                    case "ping" -> ModuleHandler.getPing(mc, builder);
+                    case "memory" -> ModuleHandler.getMemory(builder);
+                    case "biome" -> ModuleHandler.getBiome(mc, builder);
+                    case "weather" -> ModuleHandler.getWeather(mc, builder);
+                    case "position" -> ModuleHandler.getPosition(mc, builder);
+                    case "chunk" -> ModuleHandler.getChunk(mc, builder);
+                    case "dimension" -> ModuleHandler.getDimension(mc, builder);
+                    case "facing" -> ModuleHandler.getFacing(mc, builder);
+                    case "looking_at" -> ModuleHandler.getLookingAt(mc, builder);
+                    case "property" -> ModuleHandler.getProperty(mc, builder);
+                    case "target" -> ModuleHandler.getTarget(mc, builder);
+                    case "target_health" -> ModuleHandler.getTargetHealth(mc, builder);
                 }
             }
         }
